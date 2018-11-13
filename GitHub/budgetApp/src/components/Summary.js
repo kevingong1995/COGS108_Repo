8055aec78e0b5acb412a8aa085a269d5b61f45d9
Kevin Gong <kevingong1995@gmail.com>
2018-11-13 14:42:33 -0800

    edits

---
+++ GitHub/budgetApp/src/components/Summary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Summary extends React.Component{
+  render(
+  ){
+    return(
+      <div className="component-container">
+        <header>Monthly Summary</header>
+        <p>This section is for earned and spent money including a graph</p>
+      </div>
+    );
+  }
+}
+
+export default Summary;
